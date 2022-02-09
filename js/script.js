b0e70ca54bc2c5a26f663622edea70a41741c215
Daniel Kim <danielkim2711@gmail.com>
2022-02-09 21:26:19 +1300

    Fetch data from server

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const result = document.querySelector('#result');
 const filter = document.querySelector('#filter');
 const listItems = [];
+
+fetchData();
+
+async function fetchData() {
+  const response = await fetch('https://randomuser.me/api?results=50');
+  const data = await response.json();
+  console.log(data);
+}
