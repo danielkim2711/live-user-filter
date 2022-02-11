e989e5b9e04e7f36b63d23cc2b48cc96770ed6f8
Daniel Kim <danielkim2711@gmail.com>
2022-02-11 13:55:18 +1300

    Display fetched users to the screen

--- js/script.js
+++ js/script.js
@@ -6,6 +6,24 @@ fetchData();
 
 async function fetchData() {
   const response = await fetch('https://randomuser.me/api?results=50');
-  const data = await response.json();
-  console.log(data);
+  const { results } = await response.json();
+
+  // Clear results
+  results.textContent = '';
+
+  results.forEach((user) => {
+    const li = document.createElement('li');
+
+    listItems.push(li);
+
+    li.innerHTML = `
+    <img src="${user.picture.large}" alt="${user.name.first}">
+    <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+    </div>
+    `;
+
+    result.appendChild(li);
+  });
 }
