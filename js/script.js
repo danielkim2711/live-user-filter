7db8c357d9350d2e03e4860a4b156592b44d54af
Daniel Kim <danielkim2711@gmail.com>
2022-02-11 14:18:40 +1300

    Hide loading

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ async function fetchData() {
   const response = await fetch('https://randomuser.me/api?results=50');
   const { results } = await response.json();
 
-  // Clear results
-  results.textContent = '';
+  // Clear result
+  result.textContent = '';
+
+  console.log(results);
 
   results.forEach((user) => {
     const li = document.createElement('li');
@@ -20,6 +22,7 @@ async function fetchData() {
     <img src="${user.picture.large}" alt="${user.name.first}">
     <div class="user-info">
         <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.email}</p>
         <p>${user.location.city}, ${user.location.country}</p>
     </div>
     `;
